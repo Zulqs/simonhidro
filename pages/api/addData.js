1d51534a4999b3186787de818197991992964541
Zulqs <zulqs@apps.ipb.ac.id>
2024-03-28 01:10:50 +0700

    Table + AddData
    1. Table add new column
    2. AddData form feature

--- pages/api/addData.js
+++ pages/api/addData.js
@@ -17,11 +17,38 @@ export default async function handler(req, res) {
     tanggal,
   } = req.body;
 
+  let property = '-';
+  let minus = '';
+  let anomaly = false;
+  // Check for threshold conditions
+  if (parseFloat(suhu) < 22 || parseFloat(suhu) > 25) {
+    anomaly = true;
+    minus = minus+'suhu';
+  }
+  if (parseFloat(pH) < 6.0 || parseFloat(pH) > 6.5) {
+    anomaly = true;
+    if (minus !== '') {
+      minus = minus+', pH';
+    } else {
+    minus = minus+'pH';}
+  }
+  if (parseFloat(ec) < 1.0 || parseFloat(ec) > 2.0) {
+    anomaly = true;
+    if (minus !== '') {
+      minus = minus+', EC';
+    } else {
+    minus = minus+'EC';}  
+  }
+
+  if (anomaly === true) {
+    property = `Nilai ${minus} di luar ambang batas`;
+  }
+
   try {
     // Insert data into the "monitoring" table
     await sql`
-      INSERT INTO monitoring (lubang, suhu, pH, ec, berat, tinggi, daun, warnadaun, tanggal)
-      VALUES (${suhu}, ${suhu}, ${pH}, ${ec}, ${berat}, ${tinggi}, ${daun}, ${warnadaun}, ${tanggal})
+      INSERT INTO monitoring (lubang, suhu, pH, ec, berat, tinggi, daun, warnadaun, tanggal, property)
+      VALUES (${lubang}, ${suhu}, ${pH}, ${ec}, ${berat}, ${tinggi}, ${daun}, ${warnadaun}, ${tanggal}, ${property})
     `;
     return res.status(201).json({ message: 'Berhasil menambah data!' });
   } catch (error) {
